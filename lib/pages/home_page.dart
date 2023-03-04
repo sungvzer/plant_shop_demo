9644a2763a6e8aac236d28f18b79c21636b11579
sungvzer <sungvzer@proton.me>
2023-03-05 00:10:06 +0100

    fix: make home page a ListView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,8 +22,7 @@ class HomePage extends StatelessWidget {
         ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: ListView(
             children: [
               const Title(),
               const SizedBox(height: 16),
